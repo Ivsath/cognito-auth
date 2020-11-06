f20db66845b0aadeac8a791c786d837d5acf2357
Irvin Reyes <reyes.yerp@gmail.com>
2020-11-06 14:55:16 -0600

    Create layout component

---
+++ src/components/Container.js
@@ -0,0 +1,7 @@
+const Container = ({ children }) => (
+  <div style={styles.container}>{children}</div>
+)
+
+const styles = { container: { margin: '0 auto', padding: '50px 100px' } }
+
+export default Container
