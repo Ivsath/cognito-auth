f1f00963655212d400bed7df76c266586857efdd
Irvin Reyes <reyes.yerp@gmail.com>
2020-11-06 14:55:03 -0600

    Create protected component

---
+++ src/components/Protected.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import { Auth } from 'aws-amplify'
+
+import Container from './Container'
+
+function Protected(props) {
+  useEffect(() => {
+    Auth.currentAuthenticatedUser().catch(() => {
+      props.history.push('/profile')
+    })
+  }, [])
+
+  return (
+    <Container>
+      <h1>Protected route</h1>
+    </Container>
+  )
+}
+
+export default Protected
