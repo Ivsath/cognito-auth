992f76cf98424b836b0a8b173c6f14ce06bfb3c5
Irvin Reyes <reyes.yerp@gmail.com>
2020-11-06 14:56:09 -0600

    Add profile component which will also authenticate

---
+++ src/components/Profile.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import { Auth } from 'aws-amplify'
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+
+import Container from './Container'
+
+export default function Profile() {
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    checkUser()
+  }, [user])
+
+  async function checkUser() {
+    try {
+      const data = await Auth.currentUserPoolUser()
+      const userInfo = { username: data.username, ...data.attributes }
+      setUser(userInfo)
+    } catch (err) {
+      console.log('error: ', err)
+    }
+  }
+
+  return (
+    <AmplifyAuthenticator>
+      <Container>
+        <h1>Profile</h1>
+        <h2>Username: {user.username}</h2>
+        <h3>Email: {user.email}</h3> <h4>Phone: {user.phone_number}</h4>
+        <AmplifySignOut />
+      </Container>
+    </AmplifyAuthenticator>
+  )
+}
